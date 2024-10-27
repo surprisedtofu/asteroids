f28da85853314844dbfba485d333d901448119b1
surprisedtofu <wood.davers@gmail.com>
2024-10-27 11:24:09 -0600

    add shooting

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ import pygame
 import sys
 from asteroid import *
 from asteroidfield import *
-from player import *
 from constants import *
+from player import *
+from shot import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroid_group = pygame.sprite.Group()
+    shot_group = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroid_group, updatable, drawable)
-    AsteroidField.containers = (updatable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shot_group, updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
     print("Starting asteroids!")
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import circleshape
 import constants
 import pygame
+from shot import *
 
 class Player(circleshape.CircleShape):
     containers = []
@@ -35,7 +36,13 @@ class Player(circleshape.CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt * -1)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * constants.PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
