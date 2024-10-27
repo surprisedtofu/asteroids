07c1e965bbb5090017daca69f6b0cd44a2d9e21e
surprisedtofu <wood.davers@gmail.com>
2024-10-27 10:23:53 -0600

    add left-right rotation to player sprite

--- player.py
+++ player.py
@@ -18,3 +18,14 @@ class Player(circleshape.CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += constants.PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
